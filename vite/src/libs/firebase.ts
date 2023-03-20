e122e4b0173b373512bd4d2fa3abb25bee60d2f4
azusaw <azusa0304@gmail.com>
2023-03-20 15:20:56 +0000

    deploy app in firebase

--- vite/src/libs/firebase.ts
+++ vite/src/libs/firebase.ts
@@ -12,9 +12,6 @@ import {
 import { Point } from "../types/Point"
 import { User } from "../types/User"
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
