6fbf7904e3a0e850c56e2b5ac27983ce1b071f3e
Didi nGon- <ngon-@hotmail.com>
2023-03-23 16:26:11 +0000

    Add form reset option

--- vite/src/components/pages/Form/index.tsx
+++ vite/src/components/pages/Form/index.tsx
@@ -34,7 +34,7 @@ export default function PointForm() {
   const [selectedFields, setSelectedFields] = useState([])
   const deviceQuery = useQuery(
     "device-measurements",
-    () => deviceService.getDeviceMeasurements({ delay: 5000 }),
+    () => deviceService.getDeviceMeasurements({ delay: 1500 }),
     { refetchOnWindowFocus: false, enabled: false }
   )
 
@@ -51,166 +51,177 @@ export default function PointForm() {
   }, [])
 
   return (
-    <Container>
+    <>
       <Header />
-      <Paper
-        style={{
-          padding: "3rem 1.5rem",
-          margin: "0 auto",
-          maxWidth: 400,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Formik
-          initialValues={{
-            tds: 0,
-            ph: 7.0,
-            score: 5.0,
-            site: {
-              latitude: 56.9,
-              longitude: -3.4,
-            },
-          }}
-          // validationSchema={toFormikValidationSchema(formPointSchema)}
-          onSubmit={(values) => {
-            const toSubmit = {
-              score: values.score,
-              site: values.site,
-              user: user,
-            }
-            selectedFields.forEach((field) => (toSubmit[field] = values[field]))
-            savePoint(toSubmit)
-              .then(() =>
-                Swal.fire({
-                  title: "Thank you!",
-                  text: "Your data has been successfully registered.",
-                  icon: "success",
-                  showConfirmButton: false,
-                  timer: 3000,
-                })
-              )
-              .catch((err) =>
-                Swal.fire({
-                  title: "Something was wrong..",
-                  text: err,
-                  icon: "error",
-                  confirmButtonText: "close",
-                })
-              )
+      <Container maxWidth="sm">
+        <Paper
+          style={{
+            padding: "3rem 2rem",
+            margin: "0 auto",
           }}
         >
-          {({ values, setFieldValue }) => (
-            <Form>
-              <Stack direction="column" spacing={2}>
-                <Button onClick={() => deviceQuery.refetch()}>
-                  Connect to Device
-                </Button>
-                {deviceQuery.isFetching ? (
-                  <Alert severity="info">Searching for device...</Alert>
-                ) : deviceQuery.isSuccess ? (
-                  <Alert
-                    severity="info"
-                    icon={false}
-                    action={
-                      <IconButton
-                        size="small"
-                        onClick={() => {
-                          setSelectedFields(["ph", "tds"])
-                          setFieldValue("ph", deviceQuery.data.ph)
-                          setFieldValue("tds", deviceQuery.data.tds)
-                        }}
-                      >
-                        <ArrowCircleDown color="primary" />
-                      </IconButton>
-                    }
-                  >
-                    pH: {deviceQuery.data.ph.toFixed(2)} | TDS:{" "}
-                    {deviceQuery.data.tds}
-                  </Alert>
-                ) : null}
-                <FormControl>
-                  <InputLabel id="measured-values-label">
-                    Measured values
-                  </InputLabel>
-                  <SelectMui
-                    labelId="measured-values-label"
-                    multiple
-                    input={<OutlinedInput label="Measured values" />}
-                    value={selectedFields}
-                    onChange={(e) =>
-                      setSelectedFields(e.target.value as string[])
-                    }
-                    renderValue={(selected) => (
-                      <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                        {selected.map((value) => (
-                          <Chip key={value} label={value} />
-                        ))}
-                      </Box>
-                    )}
-                  >
-                    {[
-                      { field: "ph", name: "pH" },
-                      { field: "tds", name: "TDS (ppm)" },
-                    ].map(({ field, name }) => (
-                      <MenuItem key={name} value={field}>
-                        {name}
-                      </MenuItem>
-                    ))}
-                  </SelectMui>
-                </FormControl>
-                {selectedFields.includes("ph") && (
-                  <Field
-                    component={TextField}
-                    type="number"
-                    label="pH"
-                    name="ph"
-                  />
-                )}
-                {selectedFields.includes("tds") && (
-                  <Field
-                    component={TextField}
-                    type="number"
-                    label="TDS ppm"
-                    name="tds"
-                  />
-                )}
+          <Formik
+            initialValues={{
+              tds: 0,
+              ph: 7.0,
+              score: 5.0,
+              site: {
+                latitude: 56.9,
+                longitude: -3.4,
+              },
+            }}
+            // validationSchema={toFormikValidationSchema(formPointSchema)}
+            onSubmit={(values) => {
+              const toSubmit = {
+                score: values.score,
+                site: values.site,
+                user: user,
+              }
+              selectedFields.forEach(
+                (field) => (toSubmit[field] = values[field])
+              )
+              return savePoint(toSubmit)
+                .then(() =>
+                  Swal.fire({
+                    title: "Thank you!",
+                    text: "Your data has been successfully registered.",
+                    icon: "success",
+                    showConfirmButton: false,
+                    timer: 3000,
+                  })
+                )
+                .catch((err) =>
+                  Swal.fire({
+                    title: "Something was wrong..",
+                    text: err,
+                    icon: "error",
+                    confirmButtonText: "close",
+                  })
+                )
+            }}
+          >
+            {({ values, setFieldValue, handleReset }) => (
+              <Form>
+                <Stack direction="column" spacing={2} alignItems="stretch">
+                  <Button onClick={() => deviceQuery.refetch()}>
+                    Connect to Device
+                  </Button>
+                  {deviceQuery.isFetching ? (
+                    <Alert severity="info">Searching for device...</Alert>
+                  ) : deviceQuery.isSuccess ? (
+                    <Alert
+                      severity="info"
+                      icon={false}
+                      action={
+                        <IconButton
+                          size="small"
+                          onClick={() => {
+                            setSelectedFields(["ph", "tds"])
+                            setFieldValue("ph", deviceQuery.data.ph)
+                            setFieldValue("tds", deviceQuery.data.tds)
+                          }}
+                        >
+                          <ArrowCircleDown color="primary" />
+                        </IconButton>
+                      }
+                    >
+                      pH: {deviceQuery.data.ph.toFixed(2)} | TDS:{" "}
+                      {deviceQuery.data.tds}
+                    </Alert>
+                  ) : null}
+                  <FormControl>
+                    <InputLabel id="measured-values-label">
+                      Measured values
+                    </InputLabel>
+                    <SelectMui
+                      labelId="measured-values-label"
+                      multiple
+                      input={<OutlinedInput label="Measured values" />}
+                      value={selectedFields}
+                      onChange={(e) =>
+                        setSelectedFields(e.target.value as string[])
+                      }
+                      renderValue={(selected) => (
+                        <Box
+                          sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}
+                        >
+                          {selected.map((value) => (
+                            <Chip key={value} label={value} />
+                          ))}
+                        </Box>
+                      )}
+                    >
+                      {[
+                        { field: "ph", name: "pH" },
+                        { field: "tds", name: "TDS (ppm)" },
+                      ].map(({ field, name }) => (
+                        <MenuItem key={name} value={field}>
+                          {name}
+                        </MenuItem>
+                      ))}
+                    </SelectMui>
+                  </FormControl>
+                  {selectedFields.includes("ph") && (
+                    <Field
+                      component={TextField}
+                      type="number"
+                      label="pH"
+                      name="ph"
+                    />
+                  )}
+                  {selectedFields.includes("tds") && (
+                    <Field
+                      component={TextField}
+                      type="number"
+                      label="TDS ppm"
+                      name="tds"
+                    />
+                  )}
 
-                <Field
-                  component={TextField}
-                  type="number"
-                  label="Drinkability score"
-                  name="score"
-                />
-                <Field
-                  component={TextField}
-                  type="text"
-                  label="latitude"
-                  name="site.latitude"
-                />
-                <Field
-                  component={TextField}
-                  type="text"
-                  label="longitude"
-                  name="site.longitude"
-                />
-                <Map
-                  position={[values.site.latitude, values.site.longitude]}
-                  setPosition={({ lat, lng }) => {
-                    setFieldValue("site", { latitude: lat, longitude: lng })
-                  }}
-                />
-                <button type="submit" className="bubble-btn">
-                  SUBMIT
-                </button>
-                <Button onClick={() => navigate(-1)}>BACK</Button>
-              </Stack>
-            </Form>
-          )}
-        </Formik>
-      </Paper>
-    </Container>
+                  <Field
+                    component={TextField}
+                    type="number"
+                    label="Drinkability score"
+                    name="score"
+                  />
+                  <Field
+                    component={TextField}
+                    type="text"
+                    label="latitude"
+                    name="site.latitude"
+                  />
+                  <Field
+                    component={TextField}
+                    type="text"
+                    label="longitude"
+                    name="site.longitude"
+                  />
+                  <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={handleReset}
+                  >
+                    Reset
+                  </Button>
+                  <Map
+                    position={[values.site.latitude, values.site.longitude]}
+                    setPosition={({ lat, lng }) => {
+                      setFieldValue("site", { latitude: lat, longitude: lng })
+                    }}
+                  />
+                  <button type="submit" className="bubble-btn">
+                    SUBMIT
+                  </button>
+                  <Button variant="outlined" onClick={() => navigate(-1)}>
+                    BACK
+                  </Button>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
+        </Paper>
+      </Container>
+    </>
   )
 }
 
